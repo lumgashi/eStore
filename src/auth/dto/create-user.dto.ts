fd66a910fce9b0b5e0b63bea001d54ddd4c160a5
lumgashi <lumgashi97@hotmail.com>
2024-09-14 16:47:39 +0200

    added new role for user

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,7 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
 
-import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+import { UserRole } from 'src/utils/types/user-roles';
 
 export class CreateUserDto {
   @IsString({ message: 'First Name should not be string' })
@@ -24,6 +31,10 @@ export class CreateUserDto {
   @IsString()
   @ApiProperty({ example: '+00012324323' })
   phoneNumber?: string;
+
+  @IsEnum(UserRole)
+  @ApiProperty({ example: UserRole.USER })
+  role: UserRole;
 
   //@IsStrongPassword({}, { message: 'Email is not correct' })
   @IsNotEmpty({ message: 'Password should not be empty' })
