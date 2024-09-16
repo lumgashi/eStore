44066e51c980351e530b79534085f96d83f962aa
lumgashi <lumgashi97@hotmail.com>
2024-09-16 13:56:20 +0200

    fix lint

--- src/store-categories/dto/update-store-category.dto.ts
+++ src/store-categories/dto/update-store-category.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateStoreCategoryDto } from './create-store-category.dto';
 
-export class UpdateStoreCategoryDto extends PartialType(CreateStoreCategoryDto) {}
+export class UpdateStoreCategoryDto extends PartialType(
+  CreateStoreCategoryDto,
+) {}
